3b5ca4784f4171b9b9d7a273bcf5395dcb08036a
yaowenjin <1247932340@qq.com>
2020-03-26 16:02:53 +0800

    Update:lesson2

--- src/main/java/org/csu/mypetstore/MypetstoreApplication.java
+++ src/main/java/org/csu/mypetstore/MypetstoreApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MypetstoreApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(MypetstoreApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(MypetstoreApplication.class, args);
+	}
 
 }
