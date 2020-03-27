7ca12981d882e85a0b521a844db2e1547ba8f081
Zed-Wang <1072004451@qq.com>
2020-03-27 13:09:45 +0800

    Init MyPetStore Project

---
+++ src/main/java/org/csu/mypetstore/MypetstoreApplication.java
@@ -0,0 +1,13 @@
+package org.csu.mypetstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MypetstoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MypetstoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/csu/mypetstore/MypetstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package org.csu.mypetstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MypetstoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
