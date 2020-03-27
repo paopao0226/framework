8334cb1c0ad516b5b8a349560139095110f01676
Zed-Wang <1072004451@qq.com>
2020-03-27 13:58:38 +0800

    Init MyPetStore Project

---
+++ src/main/java/org/csu/mypetstore/controller/CatalogController.java
@@ -0,0 +1,17 @@
+package org.csu.mypetstore.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/catalog")
+public class CatalogController {
+
+    @GetMapping("/viewMain")
+    public String viewMain() {
+        return "catalog/main";
+    }
+
+
+}
