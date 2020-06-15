44ab776202ec67320a82274b4dbce1af5d8c108f
yaowenjin <1247932340@qq.com>
2020-06-15 14:45:56 +0800

    finished the restful part of ywj,having the privilege judgement of CMS

---
+++ src/main/java/org/csu/mypetstore/SpringBootStartApplication.java
@@ -0,0 +1,12 @@
+//package org.csu.mypetstore;
+//
+//import org.springframework.boot.builder.SpringApplicationBuilder;
+//import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+//
+//public class SpringBootStartApplication extends SpringBootServletInitializer {
+//
+//    @Override
+//    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder){
+//        return builder.sources(MypetstoreApplication.class);
+//    }
+//}
---
+++ src/main/java/org/csu/mypetstore/domain/Role.java
@@ -0,0 +1,32 @@
+package org.csu.mypetstore.domain;
+
+//角色类
+public class Role {
+    private Integer id;
+    private String username;
+    private String role;
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+}
